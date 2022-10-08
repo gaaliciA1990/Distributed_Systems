427a88f2d099f2718b42d8637de8b23c0b709ec9
Alicia Garcia <garcia.alicia1990@gmail.com>
2022-10-07 17:57:34 -0700

    Implemented connection to GCD.

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # Distributed_Systems
 Homework projects for distributes systems class
-
+</br>**Libraries**: Socket, Sys, Pickled
+</br>
+**Language**: Python3
 ## Lab 1 - Simple Client
 A simple client that connect to a Group Coordinator Daemon (GCD). The GCD responds
 with a list of potential group members which my client will send a message to. 
 The responses are printed out and then exits the server.
-**Libraries**: Socket, Sys, Pickled
-</br>
-**Language**: Python3
+
+
+## Lab 2 - Bully Algorithm
+The bully algorithm expands on lab 1. The client connects to the GCD and talks to 
+the GCD members to determine who is the leader. The leader is determined by the 
+member with the fewest days till their birthday or, if the birthday is the same, 
+the one with the smalled SU ID (int). The process listens for other members who want
+to send messages to it. If ELECTION message is received, the higher process sends
+this message to higher process's, and receives a dictionary of all group members, 
+keyed listen_address with values of corresponding process_ids. An election is 
+started to determine who is the leader. Once the leader is determined, the leader 
+sends a COORDINATOR message stating they are the winner. 
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,54 +2,92 @@
 CPSC 5520, Seattle University
 Author: Alicia Garcia
 Version: 1.0
-
-Requirements:
-    1. Have an identity which is the pair: (days until your next birthday, your SU ID)
-    2. JOIN the group by communicating with GCD (lab1)
-    3. Participate in elections
-    4. Identify when leader is not present and initiate an election (EC)
-    5. Pretend to fail every so often and subsequently recover
-
-# The "least" of two identities is the one with fewer days until the birthday, or if they have the same birthday, then
-the one with the smaller SU ID.
-
-# A process listens for other members wanting to send messages to it.
-
-# A process connects to each higher process by sending an ELECTION message to the higher process's listening server as
-described in the message protocol below.
-
-# Detecting a failed leader is done by each process sending occasional PROBE messages to her.
-
-# All messages are pickled and are a pair (message_name, message_data), where message_name is the text of the message
-name (that is, one of 'JOIN', 'ELECTION', 'COORDINATOR', or 'PROBE') and the message_data is specified in the protocol
-below or, if there is no message data, use None. Message responses, when they exist, can either be just pickled text or
-data as specified in the protocol below.
-
-# Sockets are not re-used. Once a message and its response, if it has one, are finished, the socket is closed. (For a
-commercial application, we would want to keep the sockets to avoid the reconnection overhead.)
-
-# Create your listening server with address ('localhost', 0) and then use listener.getsockname() to report your listener
-to the GCD when joining the group. The 'localhost' host name is special and usually translates to 127.0.0.1 and the port
-number of zero asks the socket library to allocate any free port for you.
-
-# Peer sockets must be non-blocking, i.e. you mustn't block waiting for the receipt of the OK when sending an ELECTION
-message (think about why this would cause our peers to think we had failed). Instead you need to poll for the response
-in conjunction with everything else you are simultaneously doing. It is recommended that you use socket.select() to do
-this.
 """
-
+from datetime import date
+import selectors
 import socket
 import pickle
 import sys
 
 
 class BullyClient:
-    def __init__(self):
-        pass
+    def __init__(self, host, port, next_bday, su_id):
+        self.listen_address = (host, int(port))
+
+        days_to_bd = (next_bday - date.today()).days  # determine the number of days till their next bday
+        self.process_id = (days_to_bd, su_id)
+
+        self.members = {}  # creating tuple for holding connected members
+        self.states = {}  # creating tuple for holding member states
+        self.leader = None  # election pending as indicated by None
+        self.selector = selectors.DefaultSelector()
+
+        # self.listener, self.listener_address = self.start_a_server()
+
+        self.BUF_SIZE = 1024
+        self.timeout = float(1.500)
+        self.failed_msg = 'Failed to connect: '
+
+    def connect(self):
+        # Set up connection with the server
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            print('Contacting GCD Server...\n')
+            # check we connect to the server successfully
+            if self.contact_server(sock, self.listen_address[0], self.listen_address[1]) is False:
+                sys.exit(1)
+
+            print('Successfully connected to GCD Server...\n')
+            join_msg= ('JOIN', (self.process_id, self.listen_address))
+            sock.sendall(pickle.dumps(join_msg))
+            self.members = pickle.loads(sock.recv(self.BUF_SIZE))
+            print(self.members)
+
+    # This function will handle the connection checks. If connection failed, return false
+    # else true
+    def contact_server(self, connection, host, port):
+        # First try the connect, if failed, we display the message and exit
+        try:
+            connection.settimeout(self.timeout)
+            connection.connect((host, port))
+            return True
+        except socket.timeout as to:
+            print(self.failed_msg, repr(to))
+            return False
+        except OSError as err:
+            print(self.failed_msg, repr(err))
+            return False
+
+    """
+    Creating a static self server to handle messages sent to my client
+    """
+    @staticmethod
+    def create_server(self):
+
+
 
 
 # Main function to run bully client program
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python3 lab1.py HOST PORT")
+    if not 4 <= len(sys.argv) <= 5:
+        print("Usage: python3 lab2.py GCDHOST GCDPORT SUID [DOB YYYY-MM-DD]")
         exit(1)
+
+    if len(sys.argv) == 5:  # if we receive a dob, format it
+        dob = sys.argv[4].split('-')
+        curr_date = date.today()
+        next_bd = date(curr_date.year, int(dob[1]), int(dob[2]))
+        if next_bd < curr_date:
+            next_bd = date(next_bd.year + 1, next_bd.month, next_bd.day)
+    else:
+        next_bd = date(2023, 1, 1)  # if no DOB is provided, set a default one
+
+    print('Next Birthday: ', next_bd)
+    suid = int(sys.argv[3])
+    print('SU ID: ', suid)
+    gcdHost = sys.argv[1]
+    gcdPort = sys.argv[2]
+
+    bully = BullyClient(gcdHost, gcdPort, next_bd, suid)
+    bully.connect()
+
+    exit(0)
