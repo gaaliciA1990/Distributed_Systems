5da055bc722aab88a4e9397918c4b51c27bcb4ab
Alicia <garcia.alicia1990@gmail.com>
2022-11-01 16:06:01 -0700

    Created class py files and added instructor code for reference

---
+++ lab4/chord_populate.py
@@ -0,0 +1,7 @@
+"""
+CPSC 5520, Seattle University
+Author: Alicia Garcia
+Version: 1.0
+
+Takes a port number of an existing node and the filename of the data file
+"""
---
+++ lab4/chord_query.py
@@ -0,0 +1,7 @@
+"""
+CPSC 5520, Seattle University
+Author: Alicia Garcia
+Version: 1.0
+
+Takes a port number of an existing node and a key (any value from column 1+4 of the file)
+"""
