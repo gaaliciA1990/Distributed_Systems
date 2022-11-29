0fa2efc1d4bb4f434eab51ddaa7bbc9a7137c240
Alicia <garcia.alicia1990@gmail.com>
2022-11-28 19:54:37 -0800

    Implemented more code for lab5

--- lab5/bitcoin_interpreter.py
+++ lab5/bitcoin_interpreter.py
@@ -6,12 +6,13 @@ Author: Alicia Garcia (copied from lab5 provided code, enhanced where needed)
 Date: 11/27/2022
 """
 from time import strftime, gmtime
+from hashlib import sha256
 
-HDR_SZ = 50
-SPACES = '  '
+HDR_SZ = 24
+SPACES = '    '
 
 
-def compactsize_t(n):
+def compactsize_t(n: int) -> bytes:
     """
     Marshalls data type of compactsize
     :param n: integer value
@@ -26,7 +27,7 @@ def compactsize_t(n):
     return uint8_t(0xff) + uint64_t(n)
 
 
-def unmarshal_compactsize(b):
+def unmarshal_compactsize(b: bytes) -> tuple:
     """
     Unmarshalls compactsize data type
     :param b: bytes
@@ -42,7 +43,7 @@ def unmarshal_compactsize(b):
     return b[0:1], unmarshal_uint(b[0:1])
 
 
-def bool_t(flag):
+def bool_t(flag: bool) -> bytes:
     """
     Marshalls boolean to unsigned 8 bit
     :param flag: boolean flag
@@ -51,7 +52,7 @@ def bool_t(flag):
     return uint8_t(1 if flag else 0)
 
 
-def ipv6_from_ipv4(ipv4_str):
+def ipv6_from_ipv4(ipv4_str: str) -> bytes:
     """
     Marshalls ipv4 string to ipv6 bytes
     :param ipv4_str: string to convert
@@ -61,7 +62,7 @@ def ipv6_from_ipv4(ipv4_str):
     return pchIPv4 + bytearray((int(x) for x in ipv4_str.split('.')))
 
 
-def ipv6_to_ipv4(ipv6):
+def ipv6_to_ipv4(ipv6: bytes) -> str:
     """
     Unmarshalls ipv6 byte to ipv4 string
     :param ipv6: byte to convert
@@ -70,7 +71,7 @@ def ipv6_to_ipv4(ipv6):
     return '.'.join([str(b) for b in ipv6[12:]])
 
 
-def uint8_t(n):
+def uint8_t(n: int) -> bytes:
     """
     Unmarshalls unsigned integer to 8 bit integer
     :param n: unsigned int
@@ -79,7 +80,7 @@ def uint8_t(n):
     return int(n).to_bytes(1, byteorder='little', signed=False)
 
 
-def uint16_t(n):
+def uint16_t(n: int) -> bytes:
     """
     Unmarshalls unsigned integer to 16 bit integer
     :param n:  unsigned int
@@ -88,7 +89,7 @@ def uint16_t(n):
     return int(n).to_bytes(2, byteorder='little', signed=False)
 
 
-def int32_t(n):
+def int32_t(n: int) -> bytes:
     """
     Marshalls 32 bit integer to unsigned
     :param n: 32 bit int
@@ -97,7 +98,7 @@ def int32_t(n):
     return int(n).to_bytes(4, byteorder='little', signed=True)
 
 
-def uint32_t(n):
+def uint32_t(n: int) -> bytes:
     """
     Unmarshalls unsigned integer to 32 bit int
     :param n:   unsiged integer
@@ -106,7 +107,7 @@ def uint32_t(n):
     return int(n).to_bytes(4, byteorder='little', signed=False)
 
 
-def int64_t(n):
+def int64_t(n: int) -> bytes:
     """
     Marshalls 64 bit integer to unsigned
     :param n: 64 bit integer
@@ -115,7 +116,7 @@ def int64_t(n):
     return int(n).to_bytes(8, byteorder='little', signed=True)
 
 
-def uint64_t(n):
+def uint64_t(n: int) -> bytes:
     """
     Unmarshalls unsigned integer to 64 bit int
     :param n: unsigned int
@@ -124,7 +125,7 @@ def uint64_t(n):
     return int(n).to_bytes(8, byteorder='little', signed=False)
 
 
-def unmarshal_int(b):
+def unmarshal_int(b: bytes) -> int:
     """
     Unmarshalls signed integer
     :param b: signed int
@@ -133,7 +134,7 @@ def unmarshal_int(b):
     return int.from_bytes(b, byteorder='little', signed=True)
 
 
-def unmarshal_uint(b):
+def unmarshal_uint(b: bytes) -> int:
     """
     Unmarshalls unsigned integer
     :param b: unsigned int
@@ -142,7 +143,7 @@ def unmarshal_uint(b):
     return int.from_bytes(b, byteorder='little', signed=False)
 
 
-def checksum(payload: bytes):
+def checksum(payload: bytes) -> int:
     """
     Calculate bitcoin protocol checksum using sha256.
     :param payload: payload bytes
@@ -151,7 +152,7 @@ def checksum(payload: bytes):
     return hash(payload)[:4]
 
 
-def swap_endian(b: bytes):
+def swap_endian(b: bytes) -> int:
     """
     Swap the endianness of the given bytes. If little, swaps to big. If big,
     swaps to little.
@@ -163,20 +164,23 @@ def swap_endian(b: bytes):
     return swapped
 
 
-def print_message(msg, text=None):
+def print_message(msg: bytes, text=None, height=None) -> str:
     """
     Report the contents of the given bitcoin message
     :param msg: bitcoin message including header
-    :return: message type
+    :param text:  Message string, optional
+    :param height: height of the local blockchain, optional
+    :return: message type string
     """
     print('\n{}MESSAGE'.format('' if text is None else (text + ' ')))
     print('({}) {}'.format(len(msg), msg[:60].hex() + ('' if len(msg) < 60 else '...')))
+
     payload = msg[HDR_SZ:]
     command = print_header(msg[:HDR_SZ], checksum(payload))
 
     if payload:
         if command == 'block':
-            header_hash = swap_endian(hash(payload[:80])).hex()
+            header_hash = swap_endian(custom_hash(payload[:80])).hex()
         else:
             header_hash = ''
         print('{}{} {}'.format(SPACES, command.upper(), header_hash))
@@ -194,11 +198,128 @@ def print_message(msg, text=None):
         print_feefilter_message(payload)
     elif command == 'getblocks':
         print_getblocks_message(payload)
-    elif command == 'inv' or command == 'getdata' or command == 'notfound':
-        print_inv_message(payload, height)
     elif command == 'block':
         print_block_message(payload)
+    elif command == 'inv' or command == 'getdata' or command == 'notfound':
+        print_inv_message(payload, height)
+    else:
+        print('Command [{}] not recognized! Terminating program'.format(command))
+
     return command
+
+
+def print_block_message(payload):
+    """
+    Prints the block message
+    :param payload: block message
+    """
+    # block headers
+    version = payload[:4]
+    prev_block = payload[4:36]
+    merkle_root = payload[36:68]
+    epoch_time = payload[68:72]
+    bits = payload[72:76]
+    nonce = payload[76:80]
+
+    block_count_bytes, block_count = unmarshal_compactsize(payload[80:])
+    transactions = payload[80 + len(block_count_bytes):]
+    prev_hash = swap_endian(prev_block)
+    merkle_hash = swap_endian(merkle_root)
+    time_str = gmtime(unmarshal_int(epoch_time))
+
+    print('{}{:32} version: {}'.format(SPACES, version.hex(), unmarshal_int(version), SPACES))
+    print("{}{:32}\n{}{:32} previous block hash\n{}".format(SPACES, prev_hash.hex()[:32], SPACES, prev_hash.hex()[32:],
+                                                            SPACES))
+    print('{}{:32}\n{}{:32} merkle root hash\n{}-'
+          .format(SPACES, merkle_hash.hex()[:32], SPACES, merkle_hash.hex()[32:], SPACES))
+    print('{}{:32} epoch time: {}'.format(SPACES, epoch_time.hex(), time_str))
+    print('{}{:32} bits: {}'.format(SPACES, bits.hex(), unmarshal_uint(bits)))
+    print('{}{:32} nonce: {}'.format(SPACES, nonce.hex(), unmarshal_uint(nonce)))
+    print('{}{:32} transaction count: {}'.format(SPACES, block_count_bytes.hex(), block_count))
+    print_transaction(transactions)
+
+
+def print_inv_message(payload, height):
+    """
+    Prints the inv message
+    :param payload: inv message
+    :param height:  local blockchaing height
+    """
+    inv_bytes, inv = unmarshal_compactsize(payload)
+    data = len(inv_bytes)
+    inventory = []
+
+    for _ in range(inv):
+        entry = payload[data:data + 4], payload[data + 4: data + 36]
+        inventory.append(entry)
+        data += 36
+
+    print('{}{:32} inv count: {}'.format(SPACES, inv_bytes.hex(), inv))
+
+    for data, (tx_type, tx_hash) in enumerate(inventory, start=height if height else 1):
+        print('\n{}{:32} type: {}\n{}-'.format(SPACES, tx_type.hex(), unmarshal_uint(tx_type), SPACES))
+        block_hash = swap_endian(tx_hash).hex()
+        print('{}{:32}\n{}{:32} block #{} hash'.format(SPACES, block_hash[:32], SPACES, block_hash[32:], data))
+
+
+def print_getblocks_message(payload):
+    """
+    Print the contents of the getblocks message
+    :param payload: getblocks message
+    """
+    version = payload[:4]
+    getblocks_bytes, getblocks_count = unmarshal_compactsize(payload[4:])
+    data = 4 + len(getblocks_bytes)
+    block_header_hashes = []
+
+    for _ in range(getblocks_count):
+        block_header_hashes.append(payload[data:data + 32])
+        data += 32
+
+    end_hash = payload[data:]
+
+    print('{}{:32} version: {}'.format(SPACES, version.hex(), unmarshal_uint(version)))
+    print('{}{:32} hash count: {}'.format(SPACES, getblocks_bytes.hex(), getblocks_count))
+
+    for header in block_header_hashes:
+        hash_hex = swap_endian(header).hex()
+        print('\n{}{:32}\n{}{:32} block header hash # {}: {}'.format(SPACES, hash_hex[:32], SPACES, hash_hex[32:], 1,
+                                                                     unmarshal_uint(header)))
+
+    end_hash_hex = end_hash.hex()
+
+    print('\n{}{:32}\n{}{:32} stop hash: {}'.format(SPACES, end_hash_hex[:32], SPACES, end_hash_hex[32:],
+                                                    unmarshal_uint(end_hash)))
+
+
+def print_feefilter_message(feerate):
+    """
+    Prints the contents of the feefilter message.
+    :param feerate: feefilter message
+    """
+    print('{}{:32} count: {}'.format(SPACES, feerate.hex(), unmarshal_uint(feerate)))
+
+
+def print_addr_message(payload):
+    """
+    Prints contects of the address message
+    :param payload: address message to print
+    """
+    address_bytes, address_count = unmarshal_compactsize(payload)
+    data = len(address_bytes)
+
+    epoch_time = payload[data:data + 4]  # extract the time from the data in ip_count
+    services = payload[data + 4:data + 12]  # extract the services info from the data in ip_count
+    ip_address = payload[data + 12:data + 28]  # extract the IP address from the data in ip_count
+    port = payload[data + 28:]  # extract the port from the data in ip_count
+
+    time_str = gmtime(unmarshal_int(epoch_time))
+
+    print('{}{:32} count: {}'.format(SPACES, address_bytes.hex(), address_count))
+    print('{}{:32} epoch time: {}'.format(SPACES, epoch_time.hex(), time_str))
+    print('{}{:32} services: {}'.format(SPACES, services.hex(), unmarshal_uint(services)))
+    print('{}{:32} host: {}'.format(SPACES, ip_address.hex(), ipv6_to_ipv4(ip_address)))
+    print('{}{:32} port: {}'.format(SPACES, port.hex(), unmarshal_uint(port)))
 
 
 def print_ping_pong_message(nonce):
@@ -206,8 +327,20 @@ def print_ping_pong_message(nonce):
     Prints contnets of ping/pong message
     :param nonce:  Payload that is always nonce
     """
-    print('{}{:32} nonce: {}'.format(SPACES * 2, nonce.hex(), unmarshal_uint(nonce)))
-    
+    print('{}{:32} nonce: {}'.format(SPACES, nonce.hex(), unmarshal_uint(nonce)))
+
+
+def print_sendcmpct_message(payload):
+    """
+    Prints contents of the sendcmpct message.
+    :param payload: sendcmpct message payload
+    """
+    announce = payload[:1]
+    version = payload[1:]
+
+    print('{}{:32} announce: {}'.format(SPACES, announce.hex(), bytes(announce) != b'\0'))
+    print('{}{:32} version: {}'.format(SPACES, version.hex(), unmarshal_uint(version)))
+
 
 def print_version_msg(b):
     """
@@ -215,9 +348,18 @@ def print_version_msg(b):
     :param b: version message contents
     """
     # pull out fields
-    version, my_services, epoch_time, your_services = b[:4], b[4:12], b[12:20], b[20:28]
-    rec_host, rec_port, my_services2, my_host, my_port = b[28:44], b[44:46], b[46:54], b[54:70], b[70:72]
+    version = b[:4]
+    my_services = b[4:12]
+    epoch_time = b[12:20]
+    your_services = b[20:28]
+
+    rec_host = b[28:44]
+    rec_port = b[44:46]
+    my_services2 = b[46:54]
+    my_host = b[54:70]
+    my_port = b[70:72]
     nonce = b[72:80]
+
     user_agent_size, uasz = unmarshal_compactsize(b[80:])
     i = 80 + len(user_agent_size)
     user_agent = b[i:i + uasz]
@@ -226,13 +368,16 @@ def print_version_msg(b):
     extra = b[i + 5:]
 
     # print report
-    prefix = '  '
+    prefix = SPACES
+
     print(prefix + 'VERSION')
     print(prefix + '-' * 56)
-    prefix *= 2
+
     print('{}{:32} version {}'.format(prefix, version.hex(), unmarshal_int(version)))
     print('{}{:32} my services'.format(prefix, my_services.hex()))
+
     time_str = strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime(unmarshal_int(epoch_time)))
+
     print('{}{:32} epoch time {}'.format(prefix, epoch_time.hex(), time_str))
     print('{}{:32} your services'.format(prefix, your_services.hex()))
     print('{}{:32} your host {}'.format(prefix, rec_host.hex(), ipv6_to_ipv4(rec_host)))
@@ -245,19 +390,9 @@ def print_version_msg(b):
     print('{}{:32} user agent \'{}\''.format(prefix, user_agent.hex(), str(user_agent, encoding='utf-8')))
     print('{}{:32} start height {}'.format(prefix, start_height.hex(), unmarshal_uint(start_height)))
     print('{}{:32} relay {}'.format(prefix, relay.hex(), bytes(relay) != b'\0'))
+
     if len(extra) > 0:
         print('{}{:32} EXTRA!!'.format(prefix, extra.hex()))
-
-
-def print_sendcmpct_message(payload):
-    """
-    Prints contents of the sendcmpct message.
-    :param payload: sendcmpct message payload
-    """
-    announce, version = payload[:1], payload[1:]
-
-    print('{}{:32} announce: {}'.format(SPACES, announce.hex(), bytes(announce) != b'\0'))
-    print('{}{:32} version: {}'.format(SPACES, version.hex(), unmarshal_uint(version)))
 
 
 def print_header(header, expected_cksum=None):
@@ -267,21 +402,96 @@ def print_header(header, expected_cksum=None):
     :param expected_cksum: the expected checksum for this version message, if known
     :return: message type
     """
-    magic, command_hex, payload_size, cksum = header[:4], header[4:16], header[16:20], header[20:]
+    magic = header[:4]
+    command_hex = header[4:16]
+    payload_size = header[16:20]
+    cksum = header[20:]
+
     command = str(bytearray([b for b in command_hex if b != 0]), encoding='utf-8')
     psz = unmarshal_uint(payload_size)
+
     if expected_cksum is None:
         verified = ''
     elif expected_cksum == cksum:
         verified = '(verified)'
     else:
         verified = '(WRONG!! ' + expected_cksum.hex() + ')'
-    prefix = '  '
+
+    prefix = SPACES
+
     print(prefix + 'HEADER')
     print(prefix + '-' * 56)
-    prefix *= 2
     print('{}{:32} magic'.format(prefix, magic.hex()))
     print('{}{:32} command: {}'.format(prefix, command_hex.hex(), command))
     print('{}{:32} payload size: {}'.format(prefix, payload_size.hex(), psz))
     print('{}{:32} checksum {}'.format(prefix, cksum.hex(), verified))
+
     return command
+
+
+def print_transaction(txn):
+    """
+    Prints the transaction content of a block
+    :param txn: transaction bytes from the block
+    """
+    # Parse the version adn transaction input count bytes
+    version = txn[:4]
+    txn_count_bytes, txn_count = unmarshal_compactsize(txn[4:])
+    data = 4 + len(txn_count_bytes)
+
+    # Parse coinbase bytes
+    coinbase_txn, coinbase_bytes_count = parse_coinbase(txn[data:], version)
+    txn_in_list = [(coinbase_txn, coinbase_bytes_count)]
+    data += len(b''.join(coinbase_txn))
+
+    # Parse transaction input bytes
+    for _ in range(1, txn_count):
+        txn_in, script_bytes_count = parse_tx_in(txn[data:])
+        txn_in_list.append((txn_in, script_bytes_count))
+        data += len(b''.join(txn_in))
+
+
+def parse_coinbase(cb_bytes, version) -> tuple:
+    """
+    Parses the bytes of a coinbase transaction
+    :param cb_bytes: coinbase transaction bytes
+    :param version:  version number of the block
+    :return:         list of the coinbase bytes, number of bytes in the script
+    """
+    hash_null = cb_bytes[:32]
+    index = cb_bytes[32:36]
+    script, script_count = unmarshal_compactsize(cb_bytes[36:])
+    data = 36 + len(script)
+
+    height = None
+    # Version 1 doesn't require height param for block [227:836]
+    if unmarshal_uint(version) > 1:
+        height = cb_bytes[data:data + 4]
+        data += 4
+
+    cb_script = cb_bytes[data:data + script_count]
+    sequence = cb_bytes[data + script_count: data + script_count + 4]
+
+    if height:
+        return [hash_null, index, script, height, cb_script, sequence], script_count
+    else:
+        return [hash_null, index, script, cb_script, sequence], script_count
+
+
+def parse_tx_in(txn_in) -> tuple:
+    """
+    Parses the transaction input bytes from a transaction
+    :param txn_in: transaction input bytes
+    :return:        list of the transaction in bytes, number of bytes in the script
+    """
+    pass
+
+
+@staticmethod
+def custom_hash(payload: bytes) -> bytes:
+    """
+    Uses sha256 to hash the payload, per BTC protocol
+    :param payload: payload message
+    :return:        hashed bytes
+    """
+    return sha256(sha256(payload).digest()).digest()
--- lab5/connect_bitcoin.py
+++ lab5/connect_bitcoin.py
@@ -8,11 +8,12 @@ Date: 11/27/2022
 import socket
 import time
 
-from lab5 import bitcoin_interpreter
+from lab5 import bitcoin_interpreter as interpreter
 
 BTC_IP = '47.40.67.209'
 HOST_IP = '127.0.0.1'
 PORT = 8333  # Mainnet
+BUFF_SZ = 64000  # buffer size for socket
 MAGIC_BYTES = bytes.fromhex('f9beb4d9')  # magic bytes
 EMPTY_STRING = ''.encode()  # empty payload
 COMMAND_SIZE = 12  # command msg length
@@ -28,7 +29,7 @@ def connect_to_btc():
 
         # send version msg, get peer version msg
         version_msg = get_build_msg('version', version_message())
-        peer_version_msg = exchange_messages(version_msg, expected_bytes=126)[0]
+        peer_version_msg = exchange_messages(btc_sock, version_msg, expected_bytes=126)[0]
 
 
 def get_build_msg(command, payload):
@@ -41,7 +42,7 @@ def get_build_msg(command, payload):
     return build_msg_header(command, payload) + payload
 
 
-def build_msg_header(command, payload):
+def build_msg_header(command: str, payload: bytes) -> bytes:
     """
     Builds a BTC message header
     :param command: type of command/message
@@ -53,8 +54,8 @@ def build_msg_header(command, payload):
     while len(command_name) < COMMAND_SIZE:
         command_name += b'\0'
 
-    payload_size = bitcoin_interpreter.uint32_t(len(payload))
-    check_sum = bitcoin_interpreter.checksum(payload)
+    payload_size = interpreter.uint32_t(len(payload))
+    check_sum = interpreter.checksum(payload)
 
     return b''.join([MAGIC_BYTES, command_name, payload_size, check_sum])
 
@@ -65,19 +66,19 @@ def version_message():
     Variables named after developer protocol
     :return: version message bytes
     """
-    version = bitcoin_interpreter.int32_t(VERSION_NUM)  # version = 700015
-    services = bitcoin_interpreter.uint64_t(0)  # Not a full node
-    timestamp = bitcoin_interpreter.int64_t(int(time.time())) # current unix epoch
-    addr_recv_services = bitcoin_interpreter.uint64_t(1)  # full node
-    addr_recv_ip_address = bitcoin_interpreter.ipv6_from_ipv4(BTC_IP)
-    addr_recv_port = bitcoin_interpreter.uint16_t(PORT)
-    addr_trans_services = bitcoin_interpreter.uint64_t(0)
-    addr_trans_ip_address = bitcoin_interpreter.ipv6_from_ipv4(HOST_IP)
-    addr_trans_port = bitcoin_interpreter.uint16_t(PORT)
-    nonce = bitcoin_interpreter.uint64_t(0)
-    user_agent_bytes = bitcoin_interpreter.compactsize_t(0)
-    start_height = bitcoin_interpreter.int32_t(0)
-    relay = bitcoin_interpreter.bool_t(False)
+    version = interpreter.int32_t(VERSION_NUM)  # version = 700015
+    services = interpreter.uint64_t(0)  # Not a full node
+    timestamp = interpreter.int64_t(int(time.time())) # current unix epoch
+    addr_recv_services = interpreter.uint64_t(1)  # full node
+    addr_recv_ip_address = interpreter.ipv6_from_ipv4(BTC_IP)
+    addr_recv_port = interpreter.uint16_t(PORT)
+    addr_trans_services = interpreter.uint64_t(0)
+    addr_trans_ip_address = interpreter.ipv6_from_ipv4(HOST_IP)
+    addr_trans_port = interpreter.uint16_t(PORT)
+    nonce = interpreter.uint64_t(0)
+    user_agent_bytes = interpreter.compactsize_t(0)
+    start_height = interpreter.int32_t(0)
+    relay = interpreter.bool_t(False)
 
     return b''.join([version, services, timestamp,
                      addr_recv_services, addr_recv_ip_address, addr_recv_port,
@@ -85,13 +86,51 @@ def version_message():
                      nonce, user_agent_bytes, start_height, relay])
 
 
-def exchange_messages(send_msg, expected_bytes=None, height=None, wait=False):
+
+
+
+def exchange_messages(btc_sock: socket, send_msg: bytes, expected_bytes=None, height=None, wait=False):
     """
     Exchanges messages with BTC node and prints the message that are being sent and received
+    :param btc_sock:       BTC socket
     :param send_msg:       bytes to send to BTC node
     :param expected_bytes: number of bytes expected to receive
     :param height:         local blockchain height
     :param wait:           Bool for whether to wait for a response
-    :return:                list of message bytes
+    :return:               list of message bytes
     """
-    pri
+    timeout = 0.5
+    interpreter.print_message(send_msg, 'send', height)
+    btc_sock.settimeout(timeout)
+    recvd_bytes = b''
+    address = (BTC_IP, PORT)
+
+    try:
+        btc_sock.send(send_msg)
+        if expected_bytes:
+            # Fix the message size
+            while len(recvd_bytes) < expected_bytes:
+                recvd_bytes += btc_sock.recv(BUFF_SZ)
+        elif wait:
+            # wait until timeout to receive all bytes
+            while True:
+                recvd_bytes += btc_sock.recv(BUFF_SZ)
+    except Exception as e:
+        print('\nNo bytes left to receive from {}: {}'.format(address, str(e)))
+
+    finally:
+        print('\nReceived {} bytes from BTC node {}'.format(len(recvd_bytes), address))
+        recvc_message_list = split_msg(recvd_bytes)
+
+
+def split_msg(peer_message: bytes) -> list:
+    """
+    Separates the bytes into a list of individual messages
+    :param peer_message: message bytes to be split
+    :return:            list of messages
+    """
+    message_list = []
+
+    while peer_message:
+        payload_size = interpreter.unmarshal_uint(peer_message[16:20])
+        msg_size = interpreter.HDR_SZ + payload_size
